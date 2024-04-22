f28bd9ca7bcaeef48b94178d4b03be917e5e18af
Moustafa <moustafaadel097@gmail.com>
2024-04-22 10:55:32 +0200

    initial mern

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const path = require("path");
 const dotenv = require("dotenv").config();
 const server = require("http").createServer();
 const mongoose = require("mongoose");
 const cors = require("cors");
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
 
 const bodyParser = require("body-parser");
 // import routes
@@ -19,11 +20,11 @@ app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true, parameterLimit: 50000 }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cookieParser())
+app.use(cookieParser());
 // cors
 app.use(
   cors({
-    origin: process.env.ORIGIN,
+    origin: ["https://oibsip-1yxf.onrender.com", process.env.ORIGIN],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     optionSuccessStatus: 200,
@@ -32,13 +33,25 @@ app.use(
   })
 );
 
-app.use("/admin", AdminRoutes);
-app.use("/user", UserRoutes);
-app.get("/", (req, res) => {
-  res.send("server is ready");
-});
-app.use(notFound);
-app.use(errorHandler);
-connectDB();
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve("../");
+  app.use(express.static(path.join(__dirname + "/frontend/build")));
 
+  app.use("/admin", AdminRoutes);
+  app.use("/user", UserRoutes);
+  app.get("/", (req, res) => {
+    res.send("server is ready");
+  });
+  app.use(notFound);
+  app.use(errorHandler);
+
+  app.get("*", (req, res, next) => {
+    return res.sendFile(
+      path.join(__dirname, "frontendmovieclient", "build", "index.html")
+    );
+  });
+}
+
+connectDB();
+mongoose.set("strictQuery", false);
 app.listen(PORT, () => console.log(`started on ${PORT}`));
